3d7e239b19046f47415924b200e9c7605c46d6ed
Aditya <adityaatanwar@gmail.com>
2023-01-11 21:52:26 +0530

    Complete the code for printing statistics

--- A1/A1.cpp
+++ A1/A1.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <random>
+#include <bits/stdc++.h>
 #include <chrono>
 
-#define ITERATIONS 5000
+#define ITERATIONS 2000
 
 using namespace std;
 
-uint64_t timeSinceEpochMillisec() {
+uint64_t get_time(){
     using namespace chrono;
     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
@@ -60,39 +57,38 @@ int pivot(vector<double> (&v), const int (&l), const int (&r), int p){
     // return p; 
 }
 
-void quick_sort(vector<double> (&v), const int (&l), const int (&r)){
+int quick_sort(vector<double> (&v), const int (&l), const int (&r)){
     // * Quick Sort the subarray v[l:r] (both ends inclusive) with the leftmost element as pivot
     if(l >= r){
-        return;
+        return 0;
     }
 
-    int p = pivot(v, l, r, l); // Default pivot is the first element in the subarray
-    quick_sort(v, l, p-1);
-    quick_sort(v, p+1, r);
-    return;
+    int c = r-l, p = pivot(v, l, r, l); // c = Number of comparisons, Default p = pivot is the first element in the subarray
+    c += quick_sort(v, l, p-1);
+    c += quick_sort(v, p+1, r);
+    return c;
 }
 
-void random_quick_sort(vector<double> (&v), const int (&l), const int (&r), uniform_int_distribution<> (&d), mt19937 (&g)){
+int random_quick_sort(vector<double> (&v), const int (&l), const int (&r), uniform_int_distribution<> (&d), mt19937 (&g)){
     // * Quick Sort the subarray v[l:r] (both ends inclusive) with random choice of pivot
     if(l >= r){
-        return;
+        return 0;
     }
 
-    int p = d(g) % (r-l+1) + l; // Choose random pivot in the inclusive range [l, r]
+    int c = r-l, p = d(g) % (r-l+1) + l; // Choose random pivot in the inclusive range [l, r]
     p = pivot(v, l, r, p);
-    quick_sort(v, l, p-1);
-    quick_sort(v, p+1, r);
-    return;
-
-    return;
+    c += quick_sort(v, l, p-1);
+    c += quick_sort(v, p+1, r);
+    return c;
 }
 
-void merge(vector<int> (&v), int i, vector<int> (&left), vector<int> (&right)){
+int merge(vector<double> (&v), int i, vector<int> (&left), vector<int> (&right)){
     // * Merging starts from v[i], ends at a suitable index. The original array v is overwritten with elements of newer arrays
     int le = 0, re = 0;                         // Number of elements read from left array, and right array
     int ls = left.size(), rs = right.size();    // Sizes of the two arrays, "left" and "right"
 
     // Standard code for merging two sorted arrays
+    int c = 0; // * Number of comparisons
     while(le < ls && re < rs){
         if(left[le] < right [re]){
             v[i] = left[le];
@@ -101,6 +97,8 @@ void merge(vector<int> (&v), int i, vector<int> (&left), vector<int> (&right)){
             v[i] = right[re];
             i++; re++;
         }
+
+        c++;
     }
 
     while(le < ls){
@@ -113,82 +111,153 @@ void merge(vector<int> (&v), int i, vector<int> (&left), vector<int> (&right)){
         i++; re++;
     }
 
-    return;
+    return c;
 }
 
-void merge_sort(vector<int> (&v), const int (&l), const int (&r)){
+int merge_sort(vector<double> (&v), const int (&l), const int (&r)){
     // * subarray v[l:r] to be sorted, both inclusive
 
     if(l >= r){
-        return;
+        return 0;
     }
 
-    int m = (l+r)>>1;
-    merge_sort(v, l, m);        // Recur for left half
-    merge_sort(v, m+1, r);      // Recur for right half
+    int m = (l+r)>>1, c = 0;
+    c += merge_sort(v, l, m);        // Recur for left half
+    c += merge_sort(v, m+1, r);      // Recur for right half
 
     vector<int> left(v.begin()+l, v.begin()+m+1), right(v.begin()+m+1, v.begin()+r+1); // Copy the two sorted halves
 
-    merge(v, l, left, right);   // Merge the two sorted halves
+    c += merge(v, l, left, right);   // Merge the two sorted halves
 
-    return;
+    return c;
 }
 
 int main(){
     int QUESTION;
     vector<int> ARRAY_SIZES = {(int)1e2, (int)1e3, (int)1e4, (int)1e5, (int)1e6};
-    cout << "Enter question number to generate data for\n";
+    cout << "Enter question number to generate data for:\n";
     cin >> QUESTION;
 
+    ofstream outfile("data3.csv");
+
     switch(QUESTION) {
+        // Quick Sort v/s Randomized Quick Sort
         case 1: 
         for (int n : ARRAY_SIZES) {
             random_device rd;   // seeding the
             mt19937 gen(rd());  // randomizer
             uniform_real_distribution<> distr_r(0, 1);  // generate a random float between 0 and 1
             uniform_int_distribution<> distr_i(0, n-1); // generate a random integer between 0 and n-1
-            vector<double> V;
+            vector<double> v, v_rqs, v_qs;
 
-            // * For Randomized Quick Sort
-            while(n--) { V.push_back(distr_r(gen)); } // generating the random array
-            n = V.size();
+            while(n--) { v.push_back(distr_r(gen)); } // generating the random array
+            n = v.size();
+            v_qs = v;
 
             for(int i = 0; i < ITERATIONS; i++) {
-                vector<double> copy_V(V);
-                int s = timeSinceEpochMillisec();
-                random_quick_sort(copy_V, 0, n-1, distr_i, gen);
-                int e = timeSinceEpochMillisec();
-
-                // file writing here
-            }
-            V.clear();
-            // ***************************
-
-            // * For Quick Sort
-            for(int i = 0; i < ITERATIONS; i++) {
-                while(n--) { V.push_back(distr_r(gen)); } // generating random array
-                n = V.size();
+                v_rqs = v;
+                shuffle(v_qs.begin(), v_qs.end(), default_random_engine(get_time()));
                 
-                int s = timeSinceEpochMillisec();
-                quick_sort(V, 0, n-1);
-                int e = timeSinceEpochMillisec();
+                int s_qs1, e_qs1, e_qs2, c_qs;
+                
+                s_qs1 = get_time();
+                c_qs = quick_sort(v_qs, 0, n-1);
+                e_qs1 = get_time();
+                quick_sort(v_qs, 0, n-1);
+                e_qs2 = get_time();
+                
+                int s_rqs1, e_rqs1, e_rqs2, c_rqs = 0;
 
-                // file writing here
-                V.clear();
+                s_rqs1 = get_time();
+                c_rqs = random_quick_sort(v_rqs, 0, n-1, distr_i, gen);
+                e_rqs1 = get_time();
+                random_quick_sort(v_rqs, 0, n-1, distr_i, gen);
+                e_rqs2 = get_time();
+
+
+                if(outfile.is_open()) {
+                    outfile << (e_qs1 - s_qs1) << "," << (e_rqs1 - s_rqs1) << "," << c_qs \
+                                               << "," << c_rqs << "," << (e_qs2 - s_qs1) 
+                                               << "," << (e_rqs2 - s_rqs1) << endl;
+                }
             }
+            
+            outfile<<endl;
         }
         break;
 
+        // Randomized Quick Sort v/s Merge Sort
         case 2:
+        for (int n : ARRAY_SIZES) {
+            random_device rd;   // seeding the
+            mt19937 gen(rd());  // randomizer
+            uniform_real_distribution<> distr_r(0, 1);  // generate a random float between 0 and 1
+            uniform_int_distribution<> distr_i(0, n-1); // generate a random integer between 0 and n-1
+            vector<double> v, v_ms, v_qs;
 
+            while(n--) { v.push_back(distr_r(gen)); } // generating the random array
+            n = v.size();
+
+            for(int i = 0; i < ITERATIONS; i++) {
+                shuffle(v.begin(), v.end(), default_random_engine(get_time()));
+                v_ms = v_qs = v;
+                
+                int s_qs1, e_qs1, c_qs;
+                
+                s_qs1 = get_time();
+                c_qs = quick_sort(v_qs, 0, n-1);
+                e_qs1 = get_time();
+                
+                int s_ms1, e_ms1, c_ms;
+
+                s_ms1 = get_time();
+                c_ms = merge_sort(v_ms, 0, n-1);
+                e_ms1 = get_time();
+
+
+                if(outfile.is_open()) {
+                    outfile << (e_qs1 - s_qs1) << "," << (e_ms1 - s_ms1) << "," << c_qs << "," << c_ms << endl;
+                }
+            }
+
+            outfile<<endl;
+        }
         break;
 
         case 3:
+        for (int n : ARRAY_SIZES) {
+            random_device rd;   // seeding the
+            mt19937 gen(rd());  // randomizer
+            uniform_real_distribution<> distr_r(0, 1);  // generate a random float between 0 and 1
+            uniform_int_distribution<> distr_i(0, n-1); // generate a random integer between 0 and n-1
+            vector<double> v, v_qs;
 
+            while(n--) { v.push_back(distr_r(gen)); } // generating the random array
+            n = v.size();
+
+            for(int i = 0; i < ITERATIONS; i++) {
+                shuffle(v.begin(), v.end(), default_random_engine(get_time()));
+                v_qs = v;
+                
+                int s_qs1, e_qs1, c_qs;
+                
+                s_qs1 = get_time();
+                c_qs = quick_sort(v_qs, 0, n-1);
+                e_qs1 = get_time();
+
+                if(outfile.is_open()) {
+                    outfile << (e_qs1 - s_qs1) << "," << c_qs << endl;
+                }
+            }
+
+            outfile<<endl;
+        }
         break;
 
         default: cout << "Invalid input\n";
     }
+
+    outfile.close();
 
     return 0;
 }
